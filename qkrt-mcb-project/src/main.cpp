557cc98f0686e0509188ed4e5b211d90d7195f35
Hunter Coker <hunter.coker.2016@gmail.com>
2024-11-15 19:57:39 -0500

    configured project to use mcb type c

---
+++ qkrt-mcb-project/src/main.cpp
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2020-2021 Queen's Knights Robotics Team <qkrt@engsoc.queensu.ca>
+ *
+ * This file is part of qkrt-mcb.
+ *
+ * qkrt-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * qkrt-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with qkrt-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifdef PLATFORM_HOSTED
+/* hosted environment (simulator) includes --------------------------------- */
+#include <iostream>
+
+#include "tap/communication/tcp-server/tcp_server.hpp"
+#include "tap/motor/motorsim/sim_handler.hpp"
+#endif
+
+#include "tap/board/board.hpp"
+
+#include "modm/architecture/interface/delay.hpp"
+
+/* arch includes ------------------------------------------------------------*/
+#include "tap/architecture/periodic_timer.hpp"
+#include "tap/architecture/profiler.hpp"
+
+/* communication includes ---------------------------------------------------*/
+#include "drivers.hpp"
+#include "drivers_singleton.hpp"
+
+/* error handling includes --------------------------------------------------*/
+#include "tap/errors/create_errors.hpp"
+
+/* control includes ---------------------------------------------------------*/
+#include "tap/architecture/clock.hpp"
+
+static constexpr float IMU_SAMPLE_FREQUENCY = 500;
+static constexpr float MAHONY_KP = 0.5f;
+static constexpr float MAHONY_KI = 0;
+
+/* define timers here -------------------------------------------------------*/
+tap::arch::PeriodicMilliTimer sendMotorTimeout(2);
+
+// Place any sort of input/output initialization here. For example, place
+// serial init stuff here.
+static void initializeIo(src::Drivers *drivers);
+
+// Anything that you would like to be called place here. It will be called
+// very frequently. Use PeriodicMilliTimers if you don't want something to be
+// called as frequently.
+static void updateIo(src::Drivers *drivers);
+
+int main()
+{
+#ifdef PLATFORM_HOSTED
+    std::cout << "Simulation starting..." << std::endl;
+#endif
+
+    /*
+     * NOTE: We are using DoNotUse_getDrivers here because in the main
+     *      robot loop we must access the singleton drivers to update
+     *      IO states and run the scheduler.
+     */
+    src::Drivers *drivers = src::DoNotUse_getDrivers();
+
+    Board::initialize();
+    initializeIo(drivers);
+
+#ifdef PLATFORM_HOSTED
+    tap::motorsim::SimHandler::resetMotorSims();
+    // Blocking call, waits until Windows Simulator connects.
+    tap::communication::TCPServer::MainServer()->getConnection();
+#endif
+
+    while (1)
+    {
+        // do this as fast as you can
+        PROFILE(drivers->profiler, updateIo, (drivers));
+
+        if (sendMotorTimeout.execute())
+        {
+            PROFILE(drivers->profiler, drivers->bmi088.periodicIMUUpdate, ());
+            PROFILE(drivers->profiler, drivers->commandScheduler.run, ());
+            PROFILE(drivers->profiler, drivers->djiMotorTxHandler.encodeAndSendCanData, ());
+            PROFILE(drivers->profiler, drivers->terminalSerial.update, ());
+        }
+        modm::delay_us(10);
+    }
+    return 0;
+}
+
+static void initializeIo(src::Drivers *drivers)
+{
+    drivers->analog.init();
+    drivers->pwm.init();
+    drivers->digital.init();
+    drivers->leds.init();
+    drivers->can.initialize();
+    drivers->errorController.init();
+    drivers->remote.initialize();
+    drivers->bmi088.initialize(IMU_SAMPLE_FREQUENCY, MAHONY_KP, MAHONY_KI);
+    drivers->bmi088.requestRecalibration();
+    drivers->refSerial.initialize();
+    drivers->terminalSerial.initialize();
+    drivers->schedulerTerminalHandler.init();
+    drivers->djiMotorTerminalSerialHandler.init();
+}
+
+static void updateIo(src::Drivers *drivers)
+{
+#ifdef PLATFORM_HOSTED
+    tap::motorsim::SimHandler::updateSims();
+#endif
+    drivers->canRxHandler.pollCanData();
+    drivers->refSerial.updateSerial();
+    drivers->remote.read();
+    drivers->bmi088.periodicIMUUpdate();
+}
