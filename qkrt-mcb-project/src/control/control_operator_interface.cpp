c77e19743b6c70ef4a81504cbc4b49adbf93636f
yqiugit <20yq21@queensu.ca>
2026-05-31 00:08:42 -0400

    added some keyboard support, need testing and config for sensitivity and speeds

--- qkrt-mcb-project/src/control/control_operator_interface.cpp
+++ qkrt-mcb-project/src/control/control_operator_interface.cpp
@@ -5,8 +5,8 @@ using tap::communication::serial::Remote;
 namespace control
 {
 
-ControlOperatorInterface::ControlOperatorInterface(tap::communication::serial::Remote& remote)
-    : m_remote(remote), m_activeDevice(DeviceType::CONTROLLER)
+ControlOperatorInterface::ControlOperatorInterface(tap::communication::serial::Remote& remote, RobotType robot)
+    : m_remote(remote), m_robot(robot)
 {
 }
 
@@ -14,26 +14,67 @@ ControlOperatorInterface::ControlOperatorInterface(tap::communication::serial::R
 
 void ControlOperatorInterface::pollInputDevices()
 {
+    handleToggles();
+
     switch (m_activeDevice)
     {
         case DeviceType::CONTROLLER:
-        
-            m_chassisXInput = std::clamp(m_remote.getChannel(Remote::Channel::LEFT_VERTICAL), -1.0f, 1.0f);
-            m_chassisYInput = std::clamp(m_remote.getChannel(Remote::Channel::LEFT_HORIZONTAL), -1.0f, 1.0f);
-            m_isChassisBey = 
-                m_remote.getChannel(tap::communication::serial::Remote::Channel::WHEEL) > 0 ? true :
-                m_remote.getChannel(tap::communication::serial::Remote::Channel::WHEEL) < 0 ? false :
-                m_isChassisBey;
+            pollController();
+            break;
 
-            m_turretPitchInput = std::clamp(m_remote.getChannel(Remote::Channel::RIGHT_VERTICAL), -1.0f, 1.0f);
-            m_turretYawInput = std::clamp(m_remote.getChannel(Remote::Channel::RIGHT_HORIZONTAL), -1.0f, 1.0f);
-
-            m_flywheelInput = m_remote.getSwitch(Remote::Switch::LEFT_SWITCH) == Remote::SwitchState::UP;
-            m_agitatorInput = m_remote.getSwitch(Remote::Switch::RIGHT_SWITCH) == Remote::SwitchState::UP;
-
-            m_autoAimInput =  m_remote.getSwitch(Remote::Switch::RIGHT_SWITCH) == Remote::SwitchState::DOWN;
-
+        case DeviceType::KEYBOARD_MOUSE:
+            pollKeyboardMouse();
+            break;
     }
+}
+
+void ControlOperatorInterface::handleToggles()
+{
+    if (m_remote.keyPressed(Remote::Key::C))
+    {
+        if(!m_toggledDevice)
+        {
+            m_activeDevice = (m_activeDevice == DeviceType::CONTROLLER) ? 
+                                DeviceType::KEYBOARD_MOUSE : DeviceType::CONTROLLER;
+            m_toggledDevice = true; 
+        }
+    }
+    else 
+    { 
+        m_toggledDevice = false; 
+    }
+
+    //TODO: agitator reversal
+}
+
+void ControlOperatorInterface::pollController()
+{
+    m_chassisXInput = std::clamp(m_remote.getChannel(Remote::Channel::LEFT_VERTICAL), -1.0f, 1.0f);
+    m_chassisYInput = std::clamp(m_remote.getChannel(Remote::Channel::LEFT_HORIZONTAL), -1.0f, 1.0f);
+    m_isChassisBey = 
+        m_remote.getChannel(tap::communication::serial::Remote::Channel::WHEEL) > 0 ? true :
+        m_remote.getChannel(tap::communication::serial::Remote::Channel::WHEEL) < 0 ? false :
+        m_isChassisBey;
+
+    m_turretPitchInput = std::clamp(m_remote.getChannel(Remote::Channel::RIGHT_VERTICAL), -1.0f, 1.0f);
+    m_turretYawInput = std::clamp(m_remote.getChannel(Remote::Channel::RIGHT_HORIZONTAL), -1.0f, 1.0f);
+
+    m_autoAimInput =  m_remote.getSwitch(Remote::Switch::RIGHT_SWITCH) == Remote::SwitchState::DOWN;
+}
+
+void ControlOperatorInterface::pollKeyboardMouse()
+{
+    float moveSpeedMultiplier = m_remote.keyPressed(Remote::Key::SHIFT) ? 4.0f : 1.5f;
+
+    // WASD mapping
+    m_chassisXInput = (m_remote.keyPressed(Remote::Key::W) - m_remote.keyPressed(Remote::Key::S)) * moveSpeedMultiplier;
+    m_chassisYInput = (m_remote.keyPressed(Remote::Key::D) - m_remote.keyPressed(Remote::Key::A)) * moveSpeedMultiplier;
+
+    // mouse mapping
+    m_turretPitchInput = -normalizeMouseTanh(m_remote.getMouseY());
+    m_turretYawInput = normalizeMouseTanh(m_remote.getMouseX());
+
+    m_isChassisBey = (m_remote.keyPressed(Remote::Key::E)) 
 }
 
 }  // control
--- qkrt-mcb-project/src/control/control_operator_interface.hpp
+++ qkrt-mcb-project/src/control/control_operator_interface.hpp
@@ -8,36 +8,50 @@ namespace control
 class ControlOperatorInterface
 {
 public:
-    ControlOperatorInterface(tap::communication::serial::Remote& remote);
+    enum class DeviceType{
+        CONTROLLER,
+        KEYBOARD_MOUSE,
+    };
+
+    enum class RobotType{
+        STANDARD,
+        HERO,
+        SENTRY,
+    };
+
+    
+    ControlOperatorInterface(tap::communication::serial::Remote& remote, RobotType robot);
     DISALLOW_COPY_AND_ASSIGN(ControlOperatorInterface)
     ~ControlOperatorInterface() = default;
 
     void pollInputDevices ();
 
+    //chassis outputs
     float getChassisXInput() const { return m_chassisXInput; };
-
     float getChassisYInput() const { return m_chassisYInput; };
-
     bool isChassisBeyblade() const { return m_isChassisBey; }; 
 
+    //turret outputs
     float getTurretPitchInput() const { return m_turretPitchInput; };
-
     float getTurretYawInput() const { return m_turretYawInput; };
-
-    bool getFlywheelInput() const { return m_flywheelInput; };
-
-    bool getAgitatorInput() const { return m_agitatorInput; };
 
     bool isAutoAim () const { return m_autoAimInput; };
     
 private:
+    void pollController();
+    void pollKeyboardMouse();
+    void handleToggles();
+
+    //dependencies
     tap::communication::serial::Remote& m_remote;
+    RobotType m_robot;
 
-    enum class DeviceType{
-        CONTROLLER,
-        KEYBOARD_MOUSE,
-    } m_activeDevice;
+    //config states
+    DeviceType m_activeDevice = DeviceType::CONTROLLER;
+    bool m_toggledDevice = false;
+    bool m_autoAimInput = false;
 
+    //control states
     float m_chassisXInput = 0.0f;
     float m_chassisYInput = 0.0f;
     bool m_isChassisBey = false;
@@ -45,10 +59,12 @@ private:
     float m_turretPitchInput = 0.0f;
     float m_turretYawInput = 0.0f;
 
-    bool m_flywheelInput = false;
-    bool m_agitatorInput = false;
-
-    bool m_autoAimInput = false;
 };
+
+inline float normalizeMouseTanh(int16_t rawMouseVal)
+{
+    constexpr float SENSITIVITY_SCALER = 0.01f;     
+    return std::tanh(static_cast<float>(rawMouseVal) * SENSITIVITY_SCALER);
+}
 
 }  // control
