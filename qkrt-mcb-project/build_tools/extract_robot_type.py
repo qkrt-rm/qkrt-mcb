160eac7cf831c2c971067322417f068637f97ee0
yqiugit <20yq21@queensu.ca>
2026-06-14 22:53:26 -0400

    added 1v1 standard profile

--- qkrt-mcb-project/build_tools/extract_robot_type.py
+++ qkrt-mcb-project/build_tools/extract_robot_type.py
@@ -19,7 +19,7 @@ from SCons.Script import *
 
 ROBOT_TYPE_FILE     = "robot-type/robot_type.hpp"
 VALID_ROBOT_TYPES   = [ "TARGET_STANDARD",
-                        "TARGET_DRONE",
+                        "TARGET_STANDARDISO",
                         "TARGET_ENGINEER",
                         "TARGET_SENTRY",
                         "TARGET_HERO" ]
